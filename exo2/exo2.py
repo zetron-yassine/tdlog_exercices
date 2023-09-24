ce6e1a32e28581896c444bdfe62c99c81ac44e85
Hicham DAKHLI <hicham.dakhli@axa.fr>
2023-09-24 20:48:12 +0200

    Exo 2

---
+++ exo2/exo2.py
@@ -0,0 +1,29 @@
+"""
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+    solution('abc', 'bc') # returns true
+    solution('abc', 'd') # returns false
+"""
+
+"""
+Create unit test using those cases:
+fixed_tests_True = (
+    ( "samurai", "ai"    ),
+    ( "ninja",   "ja"    ),
+    ( "sensei",  "i"     ),
+    ( "abc",     "abc"   ),
+    ( "abcabc",  "bc"    ),
+    ( "fails",   "ails"  ),
+)
+
+fixed_tests_False = (
+    ( "sumo",    "omo"   ),
+    ( "samurai", "ra"    ),
+    ( "abc",     "abcd"  ),
+    ( "ails",    "fails" ),
+    ( "this",    "fails" ),
+    ( "spam",    "eggs"  )
+)
+"""
